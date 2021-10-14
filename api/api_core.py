20064b6ec292c67aa7136a881ded6ebbe5e8f39b
Alexander <spier1994@gmail.com>
2021-10-14 20:33:24 +0300

    added get id

--- api/api_core.py
+++ api/api_core.py
@@ -31,6 +31,11 @@ web = 'C14D927D1E903A90CECFD838E8160785'
 class Inventory:
 
     def __init__(self, steam_id, app=753):
+        """
+        Конструктор при создании экземпляра класса Inventory
+        :param steam_id: Стим айди профиля или ссылка на профиль
+        :param app: ID приложения (например CSGO - 730)
+        """
         self.app = int(app)
         self.data = self.fetch(steam_id, app)
         if self.data.get('error'):
@@ -45,23 +50,31 @@ class Inventory:
         self.total_price = 0
 
     def fetch(self, steam_id, app, lang='russian'):
+        """
+        Запрос Steam API для получения инвентаря
+        :param steam_id: Стим айди профиля или ссылка на профиль
+        :param app: ID приложения (например CSGO - 730)
+        :param lang: Язык на котором будет получен ответ (по умолчанию "русский")
+        :return: Словарь ответа или ошибка
+        """
         context_id = app_context[app]
-        id = steam_id.isdigit()
-        if(not id):
-            received_id = requests.get(f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={web}&vanityurl={steam_id}').json()
-            steam_id = received_id.get('response').get('steamid')
+        steam_id = get_id(steam_id)
         try:
             response = requests.get(f'http://steamcommunity.com/inventory/{steam_id}/{app}/{context_id}/?l={lang}')
             if response.status_code == 403:
-                return {'error': 'Failed: Steam profile/inventory is set to private', 'items': None}
+                return {'error': 'Ошибка: Стим профиль или инвентарь запривачен', 'items': None}
             if response.status_code == 200:
                 return response.json()
             else:
-                return {'error': 'Incorrect Steam ID', 'items': None}
+                return {'error': 'Неправильный Steam ID', 'items': None}
         except:
             raise
 
     def get_inventory(self):
+        """
+        Метод полчения предметов, его перебора и извлечения основных свойств предов инвентаря
+        :return: Список объектов с предметами и их характеристиками
+        """
         if self.error:
             return self.data
         if self.total_inventory_count:
@@ -106,10 +119,19 @@ class Inventory:
             return []
 
     def inv_count(self, data):
+        """
+        Метод для подсчета кол-ва конкретного предмета
+        :param data: "classid" конкретного предмета
+        :return: Кол-во в инветаре
+        """
         item = self.get_classid()
         return item.count(data)
 
     def get_classid(self):
+        """
+        Метод получения "classid" из полученного ответа Steam API
+        :return: Список со значениями "classid"
+        """
         classid_list = []
         for id in self.assets:
             if id.get('classid'):
@@ -118,11 +140,20 @@ class Inventory:
         return classid_list
 
     def generate_bg(self):
+        """
+        Метод генерирации фона по конкретному приложению
+        :return: Ссылка
+        """
         id = self.app if self.app != '753' else 945360
         bg = f'http://cdn.akamai.steamstatic.com/steam/apps/{id}/page_bg_generated_v6b.jpg?t=1633375869'
         return bg
 
     def get_price_api(self, item_name):
+        """
+        Метод получение цены по конкретному предмету
+        :param item_name: Название предмета
+        :return: Цена или 0
+        """
         if self.price_db:
             items = self.price_db.get('items')
             for item in items:
@@ -132,6 +163,13 @@ class Inventory:
 
 
 def get_price(item_name, app_id, currency=5):
+    """
+    Функция получения цены из Торговой площадки Steam
+    :param item_name: Название предмета
+    :param app_id: ID приложения
+    :param currency: Валюта
+    :return: Цена или ошибка
+    """
     item_name = urllib.parse.quote(item_name)
     try:
         response = requests.get(
@@ -140,12 +178,17 @@ def get_price(item_name, app_id, currency=5):
         if data.get('lowest_price'):
             return data
         else:
-            return {'error': 'Request limit reached'}
+            return {'error': 'Превышен лимит запросов'}
     except ValueError as e:
         return {'error': e}
 
 
 def get_profile(steamid):
+    """
+    Метод получения основной информации профиля
+    :param steamid: Стим айди профиля или ссылка на профиль
+    :return: Словарь с данными профиля
+    """
     try:
         response = requests.get(
             f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={web}&steamids={steamid}')
@@ -162,6 +205,43 @@ def get_profile(steamid):
                 'steamid': players[0].get('steamid')
             }
         else:
-            return {'error': 'Incorrect Steam ID'}
+            return {'error': 'Непрвильный Steam ID'}
     except ValueError:
-        return {'error': 'Bad request'}
+        return {'error': 'Непрвильный запрос'}
+
+
+def get_id(steam_id):
+    """
+    Метод работы со Steam ID
+    :param steam_id: id или url
+    :return: Релевантный Steam ID
+    """
+    if steam_id.isdigit():
+        return steam_id
+    list_str = steam_id.split('/')
+
+    if len(list_str) == 1:
+        profile_id = received_id(steam_id)
+        return profile_id
+
+    for elem in list_str:
+        if elem.isdigit():
+            return elem
+
+        if elem == 'id':
+            index = list_str.index(elem)
+            profile_id = received_id(list_str[index + 1])
+            return profile_id
+
+    return steam_id
+
+
+def received_id(vanityurl):
+    """
+    Метод получения Steam ID по Steam домену
+    :param vanityurl: url
+    :return: Steam ID
+    """
+    received_res = requests.get(
+            f'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={web}&vanityurl={vanityurl}').json()
+    return received_res.get('response').get('steamid')
